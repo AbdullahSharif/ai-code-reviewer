e7aae4623f62dba132e6f9fa0c47aa67951fda04
AbdullahSharif <abdullah.29.umar@gmail.com>
2025-04-11 21:27:59 +0500

    added new func

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ console.log("Just adding a new test fiel to see what happens")
 export const printHello = async () => {
     console.log("Hello");
 }
+
+export const printStatus = async () => {
+    console.log("testing");
+}
